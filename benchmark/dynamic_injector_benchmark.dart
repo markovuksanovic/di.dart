277f687bd4561087407b81f7fb84aff4cb6a964a
Marko Vuksanovic <markovuksanovic@gmail.com>
2014-02-17 09:48:38 +1100

    Rename HttpScoreEmitter into StdoutScoreEmitter

--- benchmark/dynamic_injector_benchmark.dart
+++ benchmark/dynamic_injector_benchmark.dart
@@ -2,11 +2,11 @@ import 'package:benchmark_harness/benchmark_harness.dart';
 import 'package:di/dynamic_injector.dart';
 
 import 'injector_benchmark_common.dart';
-import 'score_emitter.dart';
+import 'emitter.dart';
 
 
 main(List<String> args) {
-  ScoreEmitter emitter = new HttpScoreEmitter();
+  ScoreEmitter emitter = new StdoutScoreEmitter();
 
   new InjectorBenchmark('DynamicInjectorBenchmark',
       (m) => new DynamicInjector(modules: m), emitter).report();
--- benchmark/emitter.dart
+++ benchmark/emitter.dart
@@ -2,9 +2,7 @@ library score_emitter;
 
 import 'package:benchmark_harness/benchmark_harness.dart';
 
-class HttpScoreEmitter implements ScoreEmitter {
-  HttpScoreEmitter() {}
-
+class StdoutScoreEmitter implements ScoreEmitter {
   void emit(String name, double value) {
     print(value);
   }
